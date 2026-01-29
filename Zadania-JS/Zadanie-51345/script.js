31cf94427d7a13b0f679485258a732bd61e683d7
omitsumoshi <lenik.agnieszka92@gmail.com>
2026-01-29 12:35:47 +0000

    Warsztat zrobiony

---
+++ Zadania-JS/Zadanie-51345/script.js
@@ -0,0 +1,8 @@
+function multiplication(num1, num2, num3){
+    let result = num1 * num2 * num3;
+    return result;
+}
+
+let multiplicationResult = multiplication(2, 3, 4);
+
+console.log(multiplicationResult);
