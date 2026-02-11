529f588acde8ea7e5038f587fcf7c9f1e1310f90
omitsumoshi <lenik.agnieszka92@gmail.com>
2026-02-11 14:41:38 +0000

    Pierwsze przeslanie

---
+++ Zadania-JS/Zadanie-51352/script.js
@@ -0,0 +1,18 @@
+
+
+let button = document.getElementsByTagName('button');
+
+const addNewLi = () => {
+    let newLi = document.createElement('li');
+    let attrNode = document.createAttribute('class');
+    attrNode.value = 'item';
+    newLi.setAttributeNode(attrNode);
+    let list = document.getElementById('items');
+    newLi.textContent = 'Item ' + (list.childElementCount + 1);
+    list.appendChild(newLi);
+
+}
+
+button.addEventListener('click', addNewLi);
+
+
