a7ed062c4f323e879f88e5cde382baa9488d7563
omitsumoshi <lenik.agnieszka92@gmail.com>
2026-02-11 13:01:40 +0000

    Zadanie zrobione

---
+++ Zadania-JS/Zadanie-51351/script.js
@@ -0,0 +1,22 @@
+
+let cities = ['Warsaw', 'Berlin', 'Paris', 'London', 'New York', 'Tokio', 'Moscow'];
+
+let olElement = document.createElement('ol');
+let txtNode = document.createTextNode('Cities:');
+
+
+const addCities = function(element) {
+
+for(let i=0; i<element.length; i++){
+    let liElement = document.createElement('li');
+    let attrNode = document.createAttribute('class');
+    attrNode.value = 'cities';
+    liElement.textContent = element[i];
+    olElement.appendChild(liElement);
+}
+    document.body.appendChild(olElement);
+
+}
+
+addCities(cities);
+
